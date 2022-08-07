aaeb10da111c388e8f6373a2e0b22736452965b0
Raphaël Margueron <raphael.margueron@gmail.com>
2022-08-08 00:24:05 +0200

    use href instead of replace to keep in history

--- app.py
+++ app.py
@@ -48,8 +48,9 @@ def main():
     return redirect("/tool")
 
 
-@app.route("/tool")
-@app.route("/tool/<region>/<summoner>/")
+@app.route("/tool/")
+@app.route("/tool/<region>")
+@app.route("/tool/<region>/<summoner>")
 def tool(region="EUW1", summoner=""):
     args = {
         "champions": enumerate(champions_keys),
