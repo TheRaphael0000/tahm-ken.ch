f1f875d6b07e22c16a0cdd14a731ea879c42b4ac
Raphaël Margueron <raphael.margueron@gmail.com>
2022-08-10 11:33:14 +0200

    update filters for compositions

--- static/js/compositions.js
+++ static/js/compositions.js
@@ -1,15 +1,19 @@
-let selects = document.querySelectorAll(".filter_select")
-let reset = document.querySelector("#reset")
+let select = document.querySelector("#filter_select")
 let compositions = document.querySelectorAll(".composition")
+let filters = document.querySelector("#filters")
+let filters_ = new Map()
 
-function filterChanged() {
-    let champions = []
-    for (let select of selects) {
-        let champion = select.selectedOptions[0].getAttribute("name")
-        if (champion != null)
-            champions.push(champion)
-    }
+function filterChanged(e) {
+    let option = e.target.selectedOptions[0]
+    filters_.set(option.value, option.innerHTML)
+    updateFilterElements()
+    filterCompositions()
+    updateFilters()
 
+    e.target.selectedIndex = 0
+}
+
+function filterCompositions() {
     for (let composition of compositions) {
         let imgs = composition.getElementsByTagName("img")
         let comp_champs = []
@@ -17,26 +21,72 @@ function filterChanged() {
             comp_champs.push(img.dataset.champion)
         }
 
-        let intersection = champions.filter(value => comp_champs.includes(value));
+        let champions = Array.from(filters_.keys())
+        let intersection = champions.filter(value => comp_champs.includes(value))
 
         if (intersection.length >= champions.length)
-            composition.style.display = "block";
+            composition.style.display = "block"
         else
-            composition.style.display = "none";
+            composition.style.display = "none"
     }
 }
 
-function resetFilters() {
-    for (let select of selects) {
-        select.selectedIndex = 0;
-        let evt = document.createEvent("HTMLEvents");
-        evt.initEvent("change", false, true);
-        select.dispatchEvent(evt);
+function updateFilterElements() {
+    filters.innerHTML = ""
+
+    for (let [champion_id, champion_name] of filters_) {
+
+        function removeFilter(id_) {
+            filters_.delete(id_)
+            updateFilterElements()
+            filterCompositions()
+            updateFilters()
+        }
+
+        let badge = document.createElement("span")
+        badge.classList.add("badge")
+        badge.classList.add("text-bg-primary")
+        badge.innerText = new DOMParser().parseFromString(champion_name, "text/html").documentElement.textContent + " "
+        badge.addEventListener("click", (e) => removeFilter(champion_id))
+
+
+        let button = document.createElement("button")
+        button.type = "button"
+        button.classList.add("btn-close")
+        button.addEventListener("click", (e) => removeFilter(champion_id))
+        badge.appendChild(button)
+
+        filters.appendChild(badge);
     }
 }
 
-for (let select of selects) {
-    select.addEventListener("change", filterChanged)
+
+select.addEventListener("change", filterChanged)
+
+
+function updateFilters() {
+    let champions_remaining = new Map()
+    for (let composition of compositions) {
+        let imgs = composition.getElementsByTagName("img")
+
+        for (let img of imgs) {
+            if (composition.style.display == "block" && !filters_.has(img.dataset.champion))
+                champions_remaining.set(img.dataset.champion, img.alt)
+        }
+    }
+
+    // clear it
+    while (select.options.length > 0) {
+        select.remove(0);
+    }
+
+    let mapAsc = new Map([...champions_remaining.entries()].sort());
+
+    // fill it
+    select.options.add(new Option("", "", true));
+    for (const [champion_id, champion_name] of mapAsc.entries()) {
+        select.options.add(new Option(champion_name, champion_id));
+    }
 }
 
-reset.addEventListener("click", resetFilters)
+updateFilters()
