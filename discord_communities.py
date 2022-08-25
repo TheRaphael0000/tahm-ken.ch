3291cffc0a54e064b53c7a453e8569401e17f122
Raphaël Margueron <raphael.margueron@gmail.com>
2022-08-25 13:36:10 +0200

    refactor discord communities

--- discord_communities.py
+++ discord_communities.py
@@ -1,30 +1,35 @@
 import datetime
 import urllib.request
-from urllib.request import Request
 import datetime
 import json
 from config import config
 
-discord_last_update = datetime.datetime(year=1, month=1, day=1)
-discord_update_delta = datetime.timedelta(hours=2)
-discord_communities = {
-    "aHs3uDraNU": {},
+# initialize to old date, to be updated on the first request
+last_update = datetime.datetime(year=1, month=1, day=1)
+update_delta = datetime.timedelta(hours=2)
+
+tahm_kench = "aHs3uDraNU"
+communities = {
+    tahm_kench: {},
     "zASN5E6RCv": {},
     "FJXAvqxw6T": {},
     "yapEVysv3b": {},
 }
 
-discord_tahm_kench = "aHs3uDraNU"
+
+def _discord_api_request(url):
+    request = urllib.request.Request(url)
+    request.add_header("Authorization", f"Bot {config['discord_bot_token']}")
+    request.add_header("User-Agent", "TKBOT (https://tahm-ken.ch, 1.0)")
+    request.add_header("Content-Type", "application/json")
+    response = urllib.request.urlopen(request)
+    return request, response
 
 
-def update_discord_server_information(invite_id, discord_community):
+def _update_discord_server_information(invite_id, discord_community):
     try:
-        invite = f"https://discord.com/api/v10/invites/{invite_id}"
-        r = Request(invite)
-        r.add_header("Authorization", f"Bot {config['discord_bot_token']}")
-        r.add_header("User-Agent", "TKBOT (https://tahm-ken.ch, 1.0)")
-        r.add_header("Content-Type", "application/json")
-        response = urllib.request.urlopen(r)
+        invite_url = f"https://discord.com/api/v10/invites/{invite_id}"
+        _, response = _discord_api_request(invite_url)
         data = response.read().decode("utf-8")
         data = json.loads(data)
         discord_community |= data
@@ -33,15 +38,14 @@ def update_discord_server_information(invite_id, discord_community):
         print(e.read())
 
 
-def update_discord():
-    for invite_id, discord_community in discord_communities.items():
-        update_discord_server_information(invite_id, discord_community)
+def _update_discord():
+    for invite_id, discord_community in communities.items():
+        _update_discord_server_information(invite_id, discord_community)
 
 
 def get_discord_communities():
-    global discord_last_update
-    if discord_last_update + discord_update_delta < datetime.datetime.now():
-        update_discord()
-        discord_last_update = datetime.datetime.now()
-
-    return discord_communities, discord_tahm_kench
+    global last_update
+    if last_update + update_delta < datetime.datetime.now():
+        _update_discord()
+        last_update = datetime.datetime.now()
+    return communities, tahm_kench
