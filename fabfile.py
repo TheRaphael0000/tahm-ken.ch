cd89df20531756068209048fe079bd78c06343a7
Raphaël Margueron <raphael.margueron@gmail.com>
2022-08-25 13:53:44 +0200

    ask less question if possible in the fabric file

--- fabfile.py
+++ fabfile.py
@@ -6,16 +6,27 @@ import fabric
 
 def deploy(c):
     with c.cd("/var/www/tahm-ken.ch/www.tahm-ken.ch"):
-        c.run("git status")
-        stash = not click.confirm("Stash and deploy to main ?", default=True)
-        upgrade = click.confirm("Upgrade modules ?", default=False)
-        if stash:
-            exit()
-        c.run("git stash")
+        current_commit = c.run("git rev-parse HEAD", hide=True).stdout.replace('\n', '')
+
+        git_status = c.run("git status --short")
+
+        if len(git_status.stdout) > 0:
+            stash = click.confirm("Stash and deploy to main ?", default=True)
+            if stash:
+                c.run("git stash")
+            else:
+                print("Deployment aborted.")
+                exit()
+
+        # update main
         c.run("git checkout main")
         c.run("git pull")
-        if upgrade:
+
+        # upgrade modules if needed
+        diff_on_requirements = c.run(f"git diff {current_commit} requirements.txt", hide=True)
+        if len(diff_on_requirements.stdout) > 0:
             c.run("pip install -r requirements.txt --upgrade")
+        
         c.run("systemctl restart www.tahm-ken.ch_gunicorn.service")
 
 
