853c8ce5b0745949d34d4ced015d49c5bdb4b31c
Raphaël Margueron <raphael.margueron@gmail.com>
2023-01-10 13:00:30 +0100

    fix the bitfield bug and use only the bitfield for challenges, use champion internal name instead

--- static/js/team_builder.js
+++ static/js/team_builder.js
@@ -26,9 +26,9 @@ let role_mapping = {
 }
 
 // selection order for the URL selection bitfield
-// APPEND NEW CHAMPIONS NAME (NOT DISPLAY NAME) AND NEW CHALLENGES ID AT THE END OF THIS LIST TO KEEP URL CONSISTENCY 
+// APPEND NEW CHALLEGNES ID AT THE END OF THIS LIST TO KEEP URL CONSISTENCY 
 let selection_order = [
-    303401, 303402, 303403, 303404, 303405, 303406, 303407, 3034080, 3034081, 3034082, 3034083, 3034084, 3034085, 303409, 303410, 303411, 303412, 303501, 303502, 303503, 303504, 303505, 303506, 303507, 303508, 303509, 303510, 303511, 303512, 303513, "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "AurelionSol", "Azir", "Bard", "Belveth", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Chogath", "Corki", "Darius", "Diana", "Draven", "DrMundo", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kaisa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Khazix", "Kindred", "Kled", "KogMaw", "KSante", "Leblanc", "LeeSin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "MonkeyKing", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "RekSai", "Rell", "Renata", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Xayah", "Xerath", "XinZhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra",
+    303401, 303402, 303403, 303404, 303405, 303406, 303407, 3034080, 3034081, 3034082, 3034083, 3034084, 3034085, 303409, 303410, 303411, 303412, 303501, 303502, 303503, 303504, 303505, 303506, 303507, 303508, 303509, 303510, 303511, 303512, 303513
 ]
 
 let table = new Tablesort(table_challenges[0], {
@@ -473,39 +473,43 @@ set_champion_size()
 btn_share.addEventListener("click", share)
 
 function encodeSelection() {
-    let bitfield = ""
+    let bitField = ""
+
+    // challenge selection order
     for (let s of selection_order) {
-        // challenges
         let isSelected
         if (typeof s == "number") {
             s = s.toString()
             isSelected = document.querySelector("#challenge_cb_" + s.substring(0, 6)).checked
-            if(s.length == 7) {
-                isSelected = challenges_select[0].selectedIndex == s.substring(6, 7)
+            if (s.length == 7) {
+                isSelected &= challenges_select[0].selectedIndex == s.substring(6, 7)
             }
         }
-        // champions
-        else if (typeof s == "string") {
-            isSelected = document.querySelector("#champion_" + s).dataset.selected == "1"
-        }
-        bitfield += isSelected ? "1" : "0"
+        bitField += isSelected ? "1" : "0"
     }
     // pad with the start with a 1 for the encoding
-    console.log(bitfield)
-    let code = encodeURL("1" + bitfield)
-    console.log(code)
+    let challengesCode = encodeURL("1" + bitField)
+
+    let selectedChampions = document.querySelectorAll(".champion_img.selected")
+    let championsCode = Array.from(selectedChampions).map((e) => {
+        return e.dataset.champion_name
+    }).toString();
+
+    let code = challengesCode + "-" + championsCode
     return code
 }
 
 
 function decodeSelection(code) {
-    code = code.replace("#", "")
+    code = code.replace("#", "").split("-")
+
+    let challengesCode = code[0]
 
     // ignore the first padded 1 used for the encoding
-    let bitfield = decodeURL(code).substring(1)
+    let bitField = decodeURL(challengesCode).substring(1)
 
     for (let i = 0; i < selection_order.length; i++) {
-        let bit = bitfield[i]
+        let bit = bitField[i]
 
         if (bit != "1")
             continue
@@ -515,7 +519,7 @@ function decodeSelection(code) {
 
         if (typeof s == "number") {
             s = s.toString()
-            if(s.length == 7) {
+            if (s.length == 7) {
                 challenges_select[0].selectedIndex = s.substring(6, 7)
             }
             node = document.querySelector("#challenge_cb_" + s.substring(0, 6))
@@ -525,6 +529,15 @@ function decodeSelection(code) {
         }
 
         node.click()
+    }
+
+    let selectedChampions = code[1]
+    selectedChampions = selectedChampions.split(",")
+
+    for (let i = 0; i < champion_img.length; i++) {
+        let node = champion_img[i]
+        if (selectedChampions.includes(node.dataset.champion_name))
+            node.click()
     }
 }
 
