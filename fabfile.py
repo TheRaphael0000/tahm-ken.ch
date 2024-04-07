ef623e786a3478a783454573f845c2b50bcb9b86
Raphaël Margueron <raphael.margueron@gmail.com>
2024-04-07 11:29:06 +0200

    add cache opgg to fabric file

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,11 @@ import fabric
 
 repo_folder = "/var/www/tahm-ken.ch/www.tahm-ken.ch"
 
+
 def deploy(c):
     with c.cd(repo_folder):
-        current_commit = c.run("git rev-parse HEAD", hide=True).stdout.replace('\n', '')
+        current_commit = c.run("git rev-parse HEAD",
+                               hide=True).stdout.replace('\n', '')
 
         git_status = c.run("git status --short")
 
@@ -24,15 +26,18 @@ def deploy(c):
         c.run("git pull")
 
         # upgrade modules if needed
-        diff_on_requirements = c.run(f"git diff {current_commit} requirements.txt", hide=True)
+        diff_on_requirements = c.run(
+            f"git diff {current_commit} requirements.txt", hide=True)
         if len(diff_on_requirements.stdout) > 0:
             c.run("pip install -r requirements.txt --upgrade")
-        
+
     with c.cd(repo_folder):
         c.run("python cache_riot_api.py")
         c.run("python cache_datadragon.py")
+        c.run("python cache_opgg.py")
 
     c.run("systemctl restart www.tahm-ken.ch_gunicorn.service")
+
 
 c = fabric.Connection(host="tahm-ken.ch", user="root", port=22)
 deploy(c)
