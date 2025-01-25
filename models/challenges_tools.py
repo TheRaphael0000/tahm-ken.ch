7591a9d08700121dfe4a10de8242995ac394b5a0
Raphaël Margueron <raphael.margueron@gmail.com>
2025-01-25 19:12:27 +0100

    fix line breaks

--- models/challenges_tools.py
+++ models/challenges_tools.py
@@ -193,8 +193,7 @@ def get_summoner_challenges_info(region, query):
 
 
 def get_champion_mastery_by_challenge(region, encryptedPUUID):
-    url = f"/lol/champion-mastery/v4/champion-masteries/by-puuid/{
-        encryptedPUUID}"
+    url = f"/lol/champion-mastery/v4/champion-masteries/by-puuid/{encryptedPUUID}"
     champions_masteries = api_request(url, region)
 
     for cm in champions_masteries:
